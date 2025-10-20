82ff8db55b4b37ebda4fa58b2bc23161ce71f9c7
Ahmad Dzaki <105299664+Dzakiki@users.noreply.github.com>
2025-10-20 12:56:41 +0000

    completed ex3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,7 +1,58 @@
 <script>
+import axios from 'axios';
     export default { 
 
        // add code here
+       data() {
+            return {
+                posts: [],
+                selectedMood: '',
+                subject: '',
+                entry: '',
+                message: ''
+            }
+       },
+       computed: {
+        baseUrl() {
+            if (window.location.hostname=='localhost')
+                return 'http://localhost:3000'
+            else {
+                const codespace_host = window.location.hostname.replace('5173', '3000')
+                return `https://${codespace_host}`;
+            }
+        }
+    },
+    created() { // created is a hook that executes as soon as Vue instance is created
+        axios.get(`${this.baseUrl}/posts`)
+            .then(response => {
+                // this gets the data, which is an array, and pass the data to Vue instance's posts property
+                this.posts = response.data
+                console.log(this.posts);
+            })
+            .catch(error => {
+                this.posts = [{ entry: 'There was an error: ' + error.message }]
+            })
+    },
+    methods: {
+        submitNewPost() {
+            const newPost = {
+                subject: this.subject,
+                entry: this.entry,
+                mood: this.selectedMood
+            };
+
+            axios.post(`${this.baseUrl}/addPost`, newPost)
+                .then(response => {
+                    this.message = 'Post added successfully!';
+                    this.subject = '';
+                    this.entry = '';
+                    this.selectedMood = '';
+                })
+                .catch(error => {
+                    this.message = 'There was an error submitting the post:' + error.message;
+                });
+        }
+    }
 
     }
 </script>
@@ -19,11 +70,15 @@
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
-
+        <select v-model="selectedMood">
+            <option v-for="post in posts">{{ post.mood }}</option>
+        </select>
         <br>
 
         <br>
-        <button>Submit New Post</button>
+        <button @click="submitNewPost">Submit New Post</button>
+
+        <p v-if="message">{{ message }}</p>
 
         <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
        
--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,7 +1,15 @@
 <script setup>
     import ViewPosts from './ViewPosts.vue';
+    import { ref } from 'vue';
+
+    const isEdit = ref(false);
+
+    
 </script>
 
 <template>
     <ViewPosts />
+    <div v-if="isEdit">
+        <h2>Edit Post</h2>
+    </div>
 </template>
